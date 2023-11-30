4c673adc8054caf10c925202b1e5e7d54dffab7c
Samantha Callie <Hoomee90@gmail.com>
2023-11-30 14:51:36 -0800

    add rowsafe method'

--- __tests__/sudoku.test.js
+++ __tests__/sudoku.test.js
@@ -1,8 +1,30 @@
-import Sudoku from "../src/js/sudoku";
+import SudokuGame from "../src/js/sudoku";
 
 describe (`sudoku`, () => {
+  let sudoku;
+  
+  beforeEach(() => {
+    sudoku = new SudokuGame();
+  });
+  
   test(`should correctly create a sudoku object`, () => {
-    const sudoku = new Sudoku();
-    expect(typeof sudoku).toBe(`object`);
+    expect(typeof sudoku).toEqual(`object`);
+  });
+
+  test(`should create a 9x9 2D array`, () => {
+    expect(sudoku.board.every((row, index) => row.every((el, index) => index <= 9) && index <= 9)).toBeTruthy();
+  });
+});
+
+describe (`safeToPlace`, () => {
+  let sudoku;
+  
+  beforeEach(() => {
+    sudoku = new SudokuGame();
+  });
+
+  test(`rowSafe`, () => {
+    expect(sudoku.rowSafe([0, 0], 0)).toBeFalsy();
+    expect(sudoku.rowSafe([8, 8], 1)).toBeTruthy();
   })
 });
--- src/js/sudoku.js
+++ src/js/sudoku.js
@@ -1,5 +1,10 @@
-export default class Sudoku {
-  constructor() {
-    
+export default class SudokuGame {
+  constructor(initialBoard = null) {
+    this.board = initialBoard || Array.from({ length: 9 }, () => Array(9).fill(0));
+  }
+
+  rowSafe(cellCords, num) {
+    let [x, y] = cellCords;
+    return !this.board[y].some(cell => cell === num)
   }
 }
