5933f01b0185dfa354e251fb4a300dd30feaf4eb
Samantha Callie <Hoomee90@gmail.com>
2023-12-01 14:06:18 -0800

    change random method to fisher yates shuffle

--- src/js/sudokuGenerator.js
+++ src/js/sudokuGenerator.js
@@ -10,20 +10,35 @@ import seeds from './sudokuSeeds.json'
 
 export default class sudokuGenerator {
   constructor(initialSeed, difficulty) {
-    this.seed = initialSeed || seeds[difficulty ? difficulty : ["easy", "medium", "hard", "vHard"].sort(() => Math.random() - 0.5).slice(0, 1)][Math.floor(Math.random() * 2)];
+    this.seed = initialSeed || seeds[difficulty ? difficulty : this.fisherYatesShuffle(["easy", "medium", "hard", "vHard"])[0]][this.fisherYatesShuffle([0, 1, 2])[0]];
+  }
+  
+  fisherYatesShuffle(deck){
+    for (var i = deck.length - 1; i > 0; i--) {
+      const swapIndex = Math.floor(Math.random() * (i + 1))
+      const currentCard = deck[i]
+      const cardToSwap = deck[swapIndex]
+      deck[i] = cardToSwap
+      deck[swapIndex] = currentCard
+    }
+    return deck
   }
 
-  rotateMatrix() {
+  rotateMatrix(matrix = this.seed) {
     let tempMatrix = Array.from(Array(9), () => Array(9));
 
     for (let i = 0; i < 9; i++) {
       for (let j = 0; j < 9; j++) {
-        let el = this.seed[i][j];
+        let el = matrix[i][j];
         let index = 9 - (i + 1)
 
         tempMatrix[j][index] = el;
       }
     }
     return tempMatrix;
+  }
+
+  mapNumbers(matrix = this.seed) {
+    
   }
 }
