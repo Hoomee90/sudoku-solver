e84675d645c1c5210efdcc77965e3416936811fc
Samantha Callie <Hoomee90@gmail.com>
2023-12-04 13:47:26 -0800

    add method to build position and memory properties to solve board

--- src/js/sudokuSolver.js
+++ src/js/sudokuSolver.js
@@ -1,6 +1,9 @@
 export default class SudokuSolver {
   constructor(initialBoard = null) {
-    this.board = initialBoard || Array.from({ length: 9 }, () => Array(9).fill(0));
+    this.board = initialBoard || Array.from(Array(9), () => Array(9).fill(0));
+    this.pos = {};
+    this.rem = {};
+    this.graph = {};
   }
 
   rowSafe(cellCords, num) {
@@ -30,5 +33,30 @@ export default class SudokuSolver {
 
   safeToPlace(cellCords, num) {
   return this.rowSafe(cellCords, num) && this.colSafe(cellCords, num) && this.boxSafe(cellCords, num);
+  }
+
+  buildPosAndRem() {
+    for (let i = 0; i < 9; i++) {
+      if (board[i][j] > 0) {
+        if (!this.pos.hasOwnProperty(board[i][j])) {
+          pos[board[i][j]] = [];
+        }
+        pos[board[i][j].push([i, j])]
+        if (!this.rem.hasOwnProperty(board[i][j])) {
+          rem[board[i][j]] = 9;
+        }
+        rem[board[i][j]] -= 9;
+      }
+    }
+
+    // Fill elements not present in pos and rem
+      for (let i = 1; i < 10; i++) {
+        if (!pos.hasOwnProperty(i)) {
+          pos[i] = [];
+      }
+      if (!rem.hasOwnProperty(i)) {
+          rem[i] = 9;
+      }
+    }
   }
 }
